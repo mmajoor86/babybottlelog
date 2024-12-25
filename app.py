a9164f53a7b014951108460276b0c1aabec80cde
Maarten Majoor <majoor@MacBook-Pro-van-Maarten.local>
2024-12-25 09:11:07 +0100

    first page working

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import page_1
+import page_2
 
-pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py")])
-pg.run()
+# Add a sidebar with options to navigate between pages
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", ["Data Entry", "Page 2"])
+
+# Depending on the selection, call the relevant page
+if selection == "Data Entry":
+    page_1.app()
+elif selection == "Page 2":
+    page_2.app()
---
+++ page_1.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+def app():
+    st.title("🍼 Baby Jessie’s Bottle Logger") 
+    st.markdown("### Log feedings and diaper changes for baby Jessie! 🌸👶")
+
+    # Record Date-Time
+    date_time = st.date_input("Date", datetime.now())
+    time = st.time_input("Time")
+    date_time = datetime.combine(date_time, time)
+
+    # Record Activity
+    activity = st.selectbox("Activity", ["🍼 Drink", "👶 Diaper"])
+
+    # Record Amount (optional)
+    amount = st.number_input("Amount Consumed (ml)", min_value=0, max_value=200, step=10, format="%d", value=0)
+
+    # Submit button
+    if st.button("Submit"):
+        st.write(f"Date-Time: {date_time}")
+        st.write(f"Activity: {activity}")
+        st.write(f"Amount Consumed: {amount} ml" if amount > 0 else "Amount Consumed: N/A")
+        store_data_csv(date_time, activity, amount)
+        st.markdown("### Updated Data 📊")
+
+
+
+def store_data_csv(date_time:datetime,
+                   activity: str,
+                   amount: int) -> pd.DataFrame:
+    formatted_date_time = date_time.strftime("%Y-%m-%d_%H-%M-%S")
+    """Store the input data as a csv file"""
+    data = { 'Date-Time': [date_time], 'Activity': [activity], 
+            'Amount Consumed': [amount if amount > 0 else None] } 
+    df = pd.DataFrame(data) 
+    df.to_csv( f'data/{formatted_date_time}.csv' , index=False)
+    return df
